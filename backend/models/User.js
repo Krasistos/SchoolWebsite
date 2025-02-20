af80571996f829a97d69bb3d5dcfafdfd22976ff
krasistos <krasikolev006@gmail.com>
2025-02-20 13:42:50 +0200

    Environment variables added, website backedn with node.js and express, user database model included,

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index').sequelize;
+
+const User = sequelize.define('User', {
+  id: { 
+    type: DataTypes.UUID, 
+    defaultValue: DataTypes.UUIDV4, 
+    primaryKey: true 
+  },
+  name: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
+  email: { 
+    type: DataTypes.STRING, 
+    allowNull: false, 
+    unique: true,
+    validate: { isEmail: true }
+  },
+  password: { 
+    type: DataTypes.STRING, 
+    allowNull: false 
+  },
+  role: { 
+    type: DataTypes.STRING, 
+    allowNull: false, 
+    defaultValue: 'user' 
+  }
+}, {
+  timestamps: true,
+  tableName: 'users'
+});
+
+module.exports = User;
