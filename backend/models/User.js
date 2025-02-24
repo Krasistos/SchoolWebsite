6c664adee88163a030647dbc4e274e49edb5af68
Krasipi4a <Krasipi4a@students.softuni.bg>
2025-02-24 18:34:27 +0200

    Some changes to db, values pluged inside .env, example User.js made, APP makes connection with backend server API

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,34 +1,34 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./index').sequelize;
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database"); // Adjusted path
 
-const User = sequelize.define('User', {
-  id: { 
-    type: DataTypes.UUID, 
-    defaultValue: DataTypes.UUIDV4, 
-    primaryKey: true 
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
   },
-  name: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  email: { 
-    type: DataTypes.STRING, 
-    allowNull: false, 
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
     unique: true,
-    validate: { isEmail: true }
+    validate: { isEmail: true },
   },
-  password: { 
-    type: DataTypes.STRING, 
-    allowNull: false 
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  role: { 
-    type: DataTypes.STRING, 
-    allowNull: false, 
-    defaultValue: 'user' 
-  }
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "user",
+  },
 }, {
   timestamps: true,
-  tableName: 'users'
+  tableName: "users",
 });
 
 module.exports = User;
